4fcb7ef0fdf9ef4510039e34b8c9f0bbce0ad85c
Juliana <julianalima@aluno.santoangelo.uri.br>
2021-01-28 17:21:08 -0300

    Curso de Navigation

---
+++ AluraEsportes/app/src/main/java/br/com/alura/aluraesporte/repository/LoginRepository.kt
@@ -0,0 +1,22 @@
+package br.com.alura.aluraesporte.repository
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+private const val CHAVE_LOGADO = "LOGADO"
+
+class LoginRepository(private val preferences: SharedPreferences) {
+
+    fun loga() = salva(true)
+
+    fun estalogado(): Boolean = preferences
+        .getBoolean(CHAVE_LOGADO, false)
+
+    fun desloga() = salva(false)
+
+    private fun salva(estado: Boolean) {
+        preferences.edit {
+            putBoolean(CHAVE_LOGADO, estado)
+        }
+    }
+}
